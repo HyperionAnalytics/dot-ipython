4b59a9008f0582be021f9301a132889bcdbf26e8
Joel Moberg <joel.moberg@gmail.com>
2014-09-17 13:14:51 +0200

    Fix search-replace ability

---
+++ nbextensions/usability/search.js
@@ -0,0 +1,8 @@
+// Simple search extension based on a codemirror addon.
+// Adds a search box to the notebook toolbar and selects search word if found.
+
+require(['/static/components/codemirror/addon/search/search.js']);
+require(['/static/components/codemirror/addon/search/searchcursor.js']);
+require(['/static/components/codemirror/addon/dialog/dialog.js']);
+require(['/static/components/codemirror/addon/dialog/dialog.css']);
+
---
+++ profile_default/static/custom/custom.js
@@ -0,0 +1,15 @@
+// Example for custom.js
+
+// we want strict javascript that fails on ambiguous syntax
+
+IPython.load_extensions('usability/search')
+IPython.load_extensions('usability/navigation-hotkeys')
+
+// IPython.keyboard_manager.command_shortcuts.add_shortcut(';', {
+//     help : 'run cell',
+//     help_index : 'zz',
+//     handler : function (event) {
+
+//         return false;
+//     }}
+// );
